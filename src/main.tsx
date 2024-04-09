834aa5aaf03396e7efd96545a3d844eb969dbff4
Alexandre TAHI <62557152+alexxtahi@users.noreply.github.com>
2024-04-10 00:35:47 +0200

    Next UI & Bootstrap icons installed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './output.css'
+import { NextUIProvider } from "@nextui-org/react";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </React.StrictMode>,
 )
